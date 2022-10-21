28857b2c8353f33333a1155c0e64871ddae0aabf
ekubay <axutec14@gmail.com>
2022-10-21 07:41:53 +0300

    train report

---
+++ scripts/train_report.py
@@ -0,0 +1,9 @@
+with open('results.txt', 'w') as result:
+      result.write('Model Performance\n')
+      result.write('\n')     
+      result.write('Linear Regression\n')
+      result.write('Best Score: 59.3%\n')
+      result.write('\n')  
+      result.write('Best Score: 24.72%\n')
+      result.write('\n')  
+      result.write('RandomForest Regression\n')
